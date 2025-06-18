1a3a8623eedfb90d62cee2dbe96c7957265da58d
Vinuka Vinnath <vinnathwlv@gmail.com>
2025-06-18 13:24:32 +0530

    Initial commit

---
+++ src/main/java/com/vinukavinnath/hellospringboot/HelloController.java
@@ -0,0 +1,13 @@
+package com.vinukavinnath.hellospringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, It's me Vinuka!";
+    }
+}
